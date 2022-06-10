5640e61682eb23e442ee40e60bfb3263358b0c82
878dm <105386865+878dm@users.noreply.github.com>
2022-06-10 15:04:50 +0500

    upload

---
+++ main.py
@@ -0,0 +1,24 @@
+
+# print( "  )" )
+# print(" (")
+# print("[" ,"]",sep="_",end=" ")
+# print(")")
+
+
+# print(" =")
+# print("/","\ ",sep=" ")
+# print(")","(",sep=" ")
+# print("[","]",sep="_")
+
+# print("(",")",sep=" _ ")
+# print(" )","(",sep=" ")
+
+# print("   *")
+# print("  *","*")
+# print(" *","*","*")
+
+
+print("   A")
+print("  B","B")
+print(" C","C","C")
+
